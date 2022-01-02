cde130608066200c8b73816e580dad57acbf4f96
Mateus Sobrinho <chocsx@MacBook-Pro-de-chocsx.local>
2022-01-02 01:39:49 -0300

    Add next-image loader

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  // images: {
+  //   loader: 'default',
+  //   path: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/',
+  // },
+  domains: ["raw.githubusercontent.com"]
 }
