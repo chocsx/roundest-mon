fbf3c57ca8c471da799feaacf19667fd64849463
Mateus Sobrinho <mateus.webphp@gmail.com>
2022-01-02 12:53:14 -0300

    WIP: trpc returning null data to front

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -1,31 +1,34 @@
-import * as trpc from '@trpc/server';
-import { z } from 'zod';
-
-import { PokemonClient } from 'pokenode-ts';
+import * as trpc from "@trpc/server";
+import { z } from "zod";
 import { prisma } from "@/backend/utils/prisma";
 
-export const appRouter = trpc.router().query("get-pokemon-by-id", {
-  input: z.object({ id: z.number() }),
-  async resolve({ input }) {
-    const pokeApiConnection = new PokemonClient();
+export const appRouter = trpc
+  .router()
+  .query("get-pokemon-by-id", {
+    input: z.object({ id: z.number() }),
+    async resolve({ input }) {
+      const pokemon = await prisma.pokemon.findFirst({
+        where: { id: input.id },
+      });
+      console.log(pokemon)
 
-    const pokemon = await pokeApiConnection.getPokemonById(input.id);
-    return {name: pokemon.name, sprites: pokemon.sprites };
-  }
-}).mutation("cast-vote", {
-  input: z.object({
-    votedFor: z.number(),
-    votedAgainst: z.number(),
-  }),
-  async resolve({ input }) {
-    const voteInDb = await prisma.vote.create({
-      data: {
-        ...input
-      }
-    })
-    return { success: true, vote: voteInDb }
-  }
-});
+      return pokemon;
+    },
+  })
+  .mutation("cast-vote", {
+    input: z.object({
+      votedFor: z.number(),
+      votedAgainst: z.number(),
+    }),
+    async resolve({ input }) {
+      const voteInDb = await prisma.vote.create({
+        data: {
+          ...input,
+        },
+      });
+      return { success: true, vote: voteInDb };
+    },
+  });
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
