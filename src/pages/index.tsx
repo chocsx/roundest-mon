898ccdc80e2a1e3ffa5ffe9dd2a4a43cf3d55e36
Mateus Sobrinho <mateus.webphp@gmail.com>
2022-01-03 00:20:27 -0300

    modify rounder button classes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 const btn =
-  "inline-flex items-center px-2.5 py1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
+  "inline-flex items-center px-3 py-1.5 border border-gray-300 shadow-sm font-medium rounded-full text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
 
 export default function Home() {
   const [ids, updateIds] = useState(() => getOptionsForVote());
